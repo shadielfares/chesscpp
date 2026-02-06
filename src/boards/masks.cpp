b1ce41da9ac59c5efee2b909626c9f6dcd9d7bee
shadielfares <shadi.elfares@gmail.com>
2026-02-06 01:56:36 -0500

    chore: added some convient masks but havent used this yet for pawn movements, will most likely need it for captures

---
+++ src/boards/masks.cpp
@@ -0,0 +1,11 @@
+// Handy Masks to have
+
+#include <cstdint>
+uint64_t file_a = 0x8080808080808080;
+uint64_t file_h = 0x101010101010101;
+
+uint64_t rank_1 = 0xff;
+uint64_t rank_8 = 0xff00000000000000;
+
+uint64_t rank_2 = 0xff00;
+uint64_t rank_7 = 0xff000000000000;
---
+++ src/boards/masks.h
@@ -0,0 +1,10 @@
+// Handy Masks to have
+#include <cstdint>
+extern uint64_t file_a; 
+extern uint64_t file_h; 
+
+extern uint64_t rank_1; 
+extern uint64_t rank_8; 
+
+extern uint64_t rank_2; 
+extern uint64_t rank_7; 
