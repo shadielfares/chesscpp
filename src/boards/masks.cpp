f01566b910ebbaf21a96b56c9b13f29365479133
octovimmer <hi@shadielfares.com>
2026-02-11 01:38:43 -0500

    fix: file_a and file_h layout mixup

--- src/boards/masks.cpp
+++ src/boards/masks.cpp
@@ -1,8 +1,8 @@
 // Handy Masks to have
 
 #include <cstdint>
-uint64_t file_a = 0x8080808080808080;
-uint64_t file_h = 0x101010101010101;
+uint64_t file_a = 0x101010101010101;
+uint64_t file_h = 0x8080808080808080;
 
 uint64_t rank_1 = 0xff;
 uint64_t rank_8 = 0xff00000000000000;
