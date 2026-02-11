2cd1d0822efc7e6f13e0dacda1f14325f210bb21
octovimmer <hi@shadielfares.com>
2026-02-11 01:39:00 -0500

    feat: added pawn_capture_generator

--- src/pieces/index.h
+++ src/pieces/index.h
@@ -1,3 +1,4 @@
 #include <cstdint>
 #include <vector>
 std::vector<uint64_t> pawn_move_generator(uint64_t board, uint64_t pawn_board, bool white_to_move);
+std::vector<uint64_t> pawn_capture_generator(uint64_t enemy_board, uint64_t pawn_board, bool white_to_move);
