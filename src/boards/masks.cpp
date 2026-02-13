3a22f9ad2f541d1d1ca2b564a23b7809106b1e14
octovimmer <hi@shadielfares.com>
2026-02-12 22:28:14 -0500

    chore: rename constants to upper case

--- src/boards/masks.cpp
+++ src/boards/masks.cpp
@@ -1,11 +1,11 @@
 // Handy Masks to have
 
 #include <cstdint>
-uint64_t file_a = 0x101010101010101;
-uint64_t file_h = 0x8080808080808080;
+uint64_t FILE_A = 0x101010101010101;
+uint64_t FILE_H = 0x8080808080808080;
 
-uint64_t rank_1 = 0xff;
-uint64_t rank_8 = 0xff00000000000000;
+uint64_t RANK_1 = 0xff;
+uint64_t RANK_8 = 0xff00000000000000;
 
-uint64_t rank_2 = 0xff00;
-uint64_t rank_7 = 0xff000000000000;
+uint64_t RANK_2 = 0xff00;
+uint64_t RANK_7 = 0xff000000000000;
